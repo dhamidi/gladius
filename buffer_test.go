51c28ccd4a3b9f3c71f2cde1eddb1caf3d1ef7ea
Dario Hamidi <dario@gowriteco.de>
2018-03-10 22:17:55 +0200

    Introduce helper method for asserting buffer contents

--- buffer_test.go
+++ buffer_test.go
@@ -2,30 +2,32 @@ package gladius
 
 import "testing"
 
+func assertBuffer(t *testing.T, b *Buffer, expected string) {
+	t.Helper()
+	t.Logf("Buffer piece table:\n%s\n", b.Inspect())
+	if contents := b.String(); contents != expected {
+		t.Fatalf("Expected %q, got %q", expected, contents)
+	}
+}
+
 func TestBuffer_InsertTextAtEnd(t *testing.T) {
 	buffer := NewBufferString("end")
 	buffer.Insert(3, "hello")
 
-	if exp, act := "endhello", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+	assertBuffer(t, buffer, "endhello")
 }
 
 func TestBuffer_InsertTextAtBeginning(t *testing.T) {
 	buffer := NewBufferString("llo")
 	buffer.Insert(0, "he")
 
-	if exp, act := "hello", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+	assertBuffer(t, buffer, "hello")
 }
 
 func TestBuffer_InsertTextInTheMiddle(t *testing.T) {
 	buffer := NewBufferString("hello world")
 	buffer.Insert(6, "cruel ")
-	if exp, act := "hello cruel world", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+	assertBuffer(t, buffer, "hello cruel world")
 }
 
 func TestBuffer_MultipleInsertsInTheMiddle(t *testing.T) {
@@ -33,9 +35,7 @@ func TestBuffer_MultipleInsertsInTheMiddle(t *testing.T) {
 	buffer.Insert(1, "1")
 	buffer.Insert(3, "2")
 
-	if exp, act := "a1b2c", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+	assertBuffer(t, buffer, "a1b2c")
 }
 
 func TestBuffer_MultipleInsertsOfDifferentTypes(t *testing.T) {
@@ -43,18 +43,13 @@ func TestBuffer_MultipleInsertsOfDifferentTypes(t *testing.T) {
 	buffer.Insert(0, "1234")
 	buffer.Insert(int64(len("abc")+len("1234")), "5678")
 	buffer.Insert(6, "!")
-	t.Logf("Buffer piece table:\n%s\n", buffer.Inspect())
-	if exp, act := "1234ab!c5678", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+
+	assertBuffer(t, buffer, "1234ab!c5678")
 }
 
 func TestBuffer_DeleteRemovesTextAtBeginning(t *testing.T) {
 	buffer := NewBufferString("abc")
 	buffer.Delete(0, 1)
-	t.Logf("Buffer piece table:\n%s\n", buffer.Inspect())
 
-	if exp, act := "bc", buffer.String(); exp != act {
-		t.Fatalf("Expected %q, got %q", exp, act)
-	}
+	assertBuffer(t, buffer, "bc")
 }
